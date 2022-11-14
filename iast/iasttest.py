63848ea48edd6d495e78d9933abd5e7c6b913051
mikewang004 <mikewang1@icloud.com>
2022-11-14 15:41:13 +0100

    started working on iast implementation

---
+++ iast/iasttest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 10:52:33 2022
+
+@author: mike
+"""
+
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import pyiast 
+
