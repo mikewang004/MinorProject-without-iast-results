8f1acedbfec9f9f15951fb38dff5343abd7d7ffe
mikewang004 <mikewang1@icloud.com>
2022-11-21 14:54:37 +0100

    modified iast

---
+++ IAST-segregated/c_python_wrapper/testiast.f
@@ -0,0 +1,83 @@
+      Program Testiast
+      Implicit None
+
+      Include 'commons.inc'
+
+      Integer J,I,Nterm_max,Carrier_gas
+      
+      Double Precision P,Yi(Maxcomp),Xi1(Maxcomp)
+     $     ,Ni(Maxcomp),Molfrac(Maxcomp,Maxterm)
+C     Note to self: program written in Fortran 2003. 
+C     An Inert Component Should Have
+C     Nterm=1
+C     Langmuir=True
+C     Ki=0
+C     Nimax Not Equal To Zero
+
+      
+      Ncomp = 3
+      Nterm_max = 2
+
+C     He
+      Ki(1,1)       = 0.0d0
+      Ki(1,2)       = 0.0d0
+      Nimax(1,1)    = 0.1d0
+      Nimax(1,2)    = 0.1d0
+      Pow(1,1)      = 1.0d0
+      Pow(1,2)      = 1.0d0
+      Langmuir(1,1) = .True.
+      Langmuir(1,2) = .True.      
+
+C     C4    
+      Ki(2,1)       = 2.26d-04
+      Ki(2,2)       = 2.35d-05
+      Nimax(2,1)    = 7.000d-01
+      Nimax(2,2)    = 1.024d+00
+      Pow(2,1)      = 1.0d0
+      Pow(2,2)      = 1.0d0
+      Langmuir(2,1) = .True.
+      Langmuir(2,2) = .True.
+      
+C     2mC3
+      Ki(3,1)       = 9.86d-05
+      Ki(3,2)       = 1.00d-07
+      Nimax(3,1)    = 7.000d-01
+      Nimax(3,2)    = 9.000d-01
+      Pow(3,1)      = 1.0d0
+      Pow(3,2)      = 1.0d0
+      Langmuir(3,1) = .True.
+      Langmuir(3,2) = .True.
+
+      Carrier_gas = 1       
+      P   = 1.0d4
+
+      Yi(1) = 0.00d0
+      Yi(2) = 0.50d0      
+      Yi(3) = 0.50d0      
+
+
+      Call Seg_Iast(Ni,Xi1,Molfrac,Yi,P,Nterm_max,Carrier_gas)
+
+      write(6,*) 'Ni(2)   ','Ni(3)   '
+      write(6,'(2e20.10)') Ni(2),Ni(3)
+
+C      stop
+
+      Do J = 0, 8
+         If(J.Lt.8) Then
+            Do I = 1, 3, 2
+               P = Dble(I*10**J)
+               Call Seg_Iast(Ni,Xi1,Molfrac,Yi,P,Nterm_max,Carrier_gas)
+               Write(25,'(5e20.10)') P,Ni(2),Ni(3)
+            Enddo
+         Else
+            P = Dble(1.0*10**J)
+            Call Seg_Iast(Ni,Xi1,Molfrac,Yi,P,Nterm_max,Carrier_gas)
+            Write(25,'(5e20.10)') P,Ni(2),Ni(3)
+         Endif
+      Enddo
+      
+      
+      Stop
+      End
+      
--- iast/iasttest.py
+++ iast/iasttest.py
@@ -11,13 +11,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pyiast 
 
-df_ch4 = pd.read_csv("/home/mike/Documents/uni/alice/Final_project/MinorProject/iast/ethaan_iso.txt")
-df_ch3ch3 = pd.read_csv("/home/mike/Documents/uni/alice/Final_project/MinorProject/iast/methane_iso2.txt")
+
+def return_langmuir(df_iso):
+    return pyiast.ModelIsotherm(df_iso, loading_key = "molkg", pressure_key = "pressure", model = "Langmuir")
+#verander onderstaand NIET omdat mijn computer te ghetto is voor een andere oplossing
+#df_ch4 = pd.read_csv("/home/mike/Documents/uni/alice/Final_project/MinorProject/iast/input/ethaan_iso.txt")
+#df_ch3ch3 = pd.read_csv("/home/mike/Documents/uni/alice/Final_project/MinorProject/iast/input/methane_iso2.txt")
+input_path = "../Raspa/outputs/C7"
+#df_ch4 = pd.read_csv("./input/ethaan_iso.txt")
+#df_ch3ch3 = pd.read_csv("./input/methane_iso2.txt")
+
+df_c7_300 = pd.read_csv(input_path + "/C7-300out.txt")
+df_c7_400 = pd.read_csv(input_path + "/C7-400out.txt")
+
+c7_300_iso = return_langmuir(df_c7_300)
+c7_400_iso = return_langmuir(df_c7_400)
 
 # Fit Langmuir isotherm to dataframe 
 
-ch4_isotherm = pyiast.ModelIsotherm(df_ch4, loading_key = "molkg", pressure_key = "pressure", model = "Langmuir")
-ch3ch3_isotherm = pyiast.ModelIsotherm(df_ch3ch3, loading_key = "molkg", pressure_key = "pressure", model = "Langmuir")
+#ch4_isotherm = pyiast.ModelIsotherm(df_ch4, loading_key = "molkg", pressure_key = "pressure", model = "Langmuir")
+#ch3ch3_isotherm = pyiast.ModelIsotherm(df_ch3ch3, loading_key = "molkg", pressure_key = "pressure", model = "Langmuir")
+
 
 #pyiast.plot_isotherm(ch3ch3_isotherm)
 #pyiast.plot_isotherm(ch4_isotherm)
@@ -25,19 +39,58 @@ ch3ch3_isotherm = pyiast.ModelIsotherm(df_ch3ch3, loading_key = "molkg", pressur
 #ch4_isotherm.print_params()
 #ch3ch3_isotherm.print_params()
 
+#Export ch4 ch3 isotherm to .txt 
+
+
 
 gas_frac = np.array([0.10, 0.90])
-total_pressure = np.linspace(0, 10e4, 50)
+#total_pressure = np.linspace(0, 10e4, 50)
 mixture = np.zeros([50, 2])
-for i in range(0,50):
-    mixture[i] = pyiast.iast(total_pressure[i] * gas_frac, [ch4_isotherm, ch3ch3_isotherm])
+#for i in range(0,50):
+#    mixture[i] = pyiast.iast(total_pressure[i] * gas_frac, [ch4_isotherm, ch3ch3_isotherm])
 
-plt.semilogx(total_pressure, mixture[:, 0], "r+", label=r"$CH_4$, IAST-approx. mixture")
-plt.semilogx(total_pressure, mixture[:, 1], "g+", label=r"$CH_3-CH_3$, IAST-approx. mixture")
-plt.semilogx(total_pressure, ch4_isotherm.loading(total_pressure), "ro", label=r"$CH_4$, homogeneous gas")
-plt.semilogx(total_pressure, ch3ch3_isotherm.loading(total_pressure), "go", label=r"$CH_3-CH_3$, homogeneous gas")
-plt.title("Loading of a %1.0f - %1.0f percent methane/ethane mixture" %(gas_frac[0]*100, gas_frac[1]*100))
+
+#plt.semilogx(total_pressure, mixture[:, 0], "r+", label=r"$CH_4$, IAST-approx. mixture")
+#plt.semilogx(total_pressure, mixture[:, 1], "g+", label=r"$CH_3-CH_3$, IAST-approx. mixture")
+#plt.semilogx(total_pressure, ch4_isotherm.loading(total_pressure), "ro", label=r"$CH_4$, homogeneous gas")
+#plt.semilogx(total_pressure, ch3ch3_isotherm.loading(total_pressure), "go", label=r"$CH_3-CH_3$, homogeneous gas")
+#plt.title("Loading of a %1.0f - %1.0f percent methane/ethane mixture" %(gas_frac[0]*100, gas_frac[1]*100))
+#plt.xlabel("Pressure (bar)")
+#plt.ylabel("Loading (mol/kg)")
+#plt.legend()
+#plt.show()
+
+# Now doing the same but for multiple mixtures: 
+no_fracs = 10
+no_pressures = 50
+gas_frac = np.linspace(0.0, 0.05, no_fracs)
+partial_pressures = np.linspace(0, 10e5, no_pressures)
+mix_isotherm = np.zeros((no_fracs, no_pressures, 2))
+for i in range(0, no_fracs):
+    for j in range(0, no_pressures):
+        mix_isotherm[i, j] = pyiast.iast(partial_pressures[j] * np.array([gas_frac[i], 1-  gas_frac[i]]), [c7_300_iso, c7_400_iso])
+
+
+plt.figure()
+for i in range(1, no_fracs-1):
+    plt.semilogx(partial_pressures, mix_isotherm[i,:, 0], "r+")
+    plt.semilogx(partial_pressures, mix_isotherm[i,:, 1], "g+")
+plt.semilogx(partial_pressures, mix_isotherm[1,:, 0], "rv", label=r"$CH_7-300$, lowest fraction")
+plt.semilogx(partial_pressures, mix_isotherm[1,:, 1], "gv", label=r"$CH_7-400$, highest fraction")
+plt.semilogx(partial_pressures, mix_isotherm[no_fracs-1,:, 0], "r^", label=r"$CH_7-300$, highest fraction")
+plt.semilogx(partial_pressures, mix_isotherm[no_fracs-1,:, 1], "g^", label=r"$CH_7-400$, lowest fraction")
+plt.semilogx(partial_pressures, c7_300_iso.loading(partial_pressures), "ro", label=r"$CH_7-300$, homogeneous gas")
+plt.semilogx(partial_pressures, c7_400_iso.loading(partial_pressures), "go", label=r"$CH_7-400$, homogeneous gas")
+plt.title("Loadings of various ethane/methane mixtures")
 plt.xlabel("Pressure (bar)")
 plt.ylabel("Loading (mol/kg)")
 plt.legend()
 plt.show()
+
+
+#Write results to an output file
+
+np.savetxt("output/pressures_gas_frac.txt", partial_pressures)
+np.savetxt("output/mixing_fractures.txt", gas_frac)
+np.savetxt("output/mix_isothermc7-300.csv", mix_isotherm[:,:,0])
+np.savetxt("output/mix_isothermc7-500.csv", mix_isotherm[:,:,1])
