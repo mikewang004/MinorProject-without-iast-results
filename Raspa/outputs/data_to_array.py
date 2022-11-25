c974c978a44c841f587686500dfccaf3e22e43df
Dariya <dariyachobanova@gmail.com>
2022-11-25 16:39:01 +0100

    update data inladen

--- Raspa/outputs/data_to_array.py
+++ Raspa/outputs/data_to_array.py
@@ -1,5 +1,19 @@
-
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
+#Convert txt files to np arrays
+directory="C7"
+file=directory + "-400out.txt"
+filename= directory + "/" + file
 
-pressure,muc,muc_err,molkg,molkg_err,_=np.loadtxt("2mC6/2mC6-400out.txt",delimiter=",",unpack=True)
+molecule=np.genfromtxt(filename,delimiter=",",names=True,usecols=(0,1,2,3,4))
+p=molecule['pressure']
+muc=molecule['muc']
+
+plt.figure()
+plt.title(file[:-7]+' molecules per unit cell')
+plt.xlabel('Pressure')
+plt.ylabel('muc')
+plt.semilogx(p,muc,'ko')
+plt.show()
+# %%
