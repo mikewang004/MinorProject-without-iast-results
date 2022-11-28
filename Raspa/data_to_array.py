2a85314ed4fa1abe7a6a6da7915bded9a430ced6
Dariya <dariyachobanova@gmail.com>
2022-11-28 11:38:36 +0100

    dara_to_array kan je negeren, ik was dingen aan t proberen

--- Raspa/data_to_array.py
+++ Raspa/data_to_array.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
 #Convert txt files to np arrays
-directory="C7"
-file=directory + "-400out.txt"
+directory="2mC6"
+file=directory + "-500out.txt"
 filename= directory + "/" + file
 
 molecule=np.genfromtxt(filename,delimiter=",",names=True,usecols=(0,1,2,3,4))
@@ -14,6 +16,5 @@ plt.figure()
 plt.title(file[:-7]+' molecules per unit cell')
 plt.xlabel('Pressure')
 plt.ylabel('muc')
-plt.semilogx(p,muc,'ko')
+plt.semilogx(p,muc,'ko',linestyle='dashed')
 plt.show()
-# %%
