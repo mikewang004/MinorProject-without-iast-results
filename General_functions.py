a1e8cdc9ee33dda0065c71fb7a99a6d3a0bc6903
WAlbers99 <78804185+WAlbers99@users.noreply.github.com>
2022-11-28 12:23:34 +0100

    Update General_functions.py

--- General_functions.py
+++ General_functions.py
@@ -52,7 +52,7 @@ def data_gathering(path_to_output):
                     paths =  mappath + "/" + str(file)
                     label = file.split("out")[0]
                     df = pd.read_table(paths, delimiter = ",")
-                    df = df.set_index("pressure")
+                    #df = df.set_index("pressure")
                     data[label] = df.drop("_", axis = 1)
                 except:
                     print("ERROR !!!, please check " + file + " \n")
