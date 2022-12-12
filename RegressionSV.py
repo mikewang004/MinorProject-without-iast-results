68f655f8c34d510308fa93fe32106d8bfd3f8022
WAlbers99 <78804185+WAlbers99@users.noreply.github.com>
2022-12-12 21:52:14 +0100

    Fixed a bug in make_iast_database_Wessel_version
    The function now works up till 5 molecule mixture for sure. Probably also for higher number molecule mixtures

--- RegressionSV.py
+++ RegressionSV.py
@@ -5,23 +5,18 @@ Created on Sun Nov 27 16:44:10 2022
 @author: Wesse
 """
 
-import sklearn.svm.SVR as SVR
 from sklearn.model_selection import train_test_split  
-
+import tensorflow as tf
 from General_functions import ML_database ,simple_database, make_IAST_database_Wessel_version
 import numpy as np
 
-#inputs
-path_to_output = "Raspa/outputs"
 
 #making database of molecules representation
 selfies_database = ML_database()
 easy_database = simple_database()
 
 #getting IAST combined with the molecule representaion
-x_data, y_data = make_IAST_database_Wessel_version(easy_database,2)
+x_data, y_data = make_IAST_database_Wessel_version(easy_database,3)
 
-x_train, x_test, y_train, y_test = train_test_split(x_data,y_data,test_size= 0.3)
-
-
+#x_train, x_test, y_train, y_test = train_test_split(x_data,y_data,test_size= 0.3)
 
