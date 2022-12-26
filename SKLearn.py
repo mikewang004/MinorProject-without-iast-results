3793ae8797a8baca67b595755911d5b458f2ef44
Steven-dR119 <ibvc119@gmail.com>
2022-12-26 09:56:40 +0100

    Comparison SKLearn

--- SKLearn.py
+++ SKLearn.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import sklearn
 from sklearn.neural_network import MLPClassifier
 from sklearn.neural_network import MLPRegressor
-
+import time
 
 # Import necessary modules
 from sklearn.model_selection import train_test_split
@@ -29,18 +29,22 @@ x_data, y_data = make_IAST_database_Wessel_version(easy_database,3)
 x_train, x_test, y_train, y_test = train_test_split(x_data,y_data,test_size= 0.3)
 
 print(x_train.shape); print(x_test.shape)
-
+start = time.time()
 model = Sequential()
-model.add(Dense(50, input_dim=14, activation= "sigmoid"))
+model.add(Dense(50, input_dim=29, activation= "sigmoid"))
 model.add(Dense(100, activation= "sigmoid"))
 model.add(Dense(150, activation= "sigmoid"))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(50, activation= "sigmoid"))
+#model.add(Dense(300, activation= "sigmoid"))
+#model.add(Dense(200, activation='relu'))
+model.add(Dense(150, activation= "relu"))
+model.add(Dense(100, activation="sigmoid"))
 model.add(Dense(3))
 
-model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
-model.fit(x_train, y_train, epochs=20)
 
+model.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
+model.fit(x_train, y_train, epochs=30)
+end = time.time()
+print("Model training took: ",end-start,"[sec]")
 pred_train= model.predict(x_train)
 print(np.sqrt(mean_squared_error(y_train,pred_train)))
 
