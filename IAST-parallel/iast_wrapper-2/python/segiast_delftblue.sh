1cdd9df13f76e36df64fd35c06958f4b93597730
mikewang004 <mikewang1@icloud.com>
2023-01-05 12:01:37 +0100

    bug fixing

--- IAST-parallel/iast_wrapper-2/python/segiast_delftblue.sh
+++ IAST-parallel/iast_wrapper-2/python/segiast_delftblue.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=compute
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=16GB
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8GB
 
 #SBATCH -N 1
 #SBATCH --export=ALL
